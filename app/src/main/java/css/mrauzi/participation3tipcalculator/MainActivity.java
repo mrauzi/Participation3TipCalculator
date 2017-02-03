09a55ed1719ae122537a43fad5eee7251867c0ae
mrauzi <mike.rauzi@gmail.com>
2017-02-03 11:38:19 -0600

    Initial commit

---
+++ app/src/main/java/css/mrauzi/participation3tipcalculator/MainActivity.java
@@ -0,0 +1,42 @@
+package css.mrauzi.participation3tipcalculator;
+
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // declare variables here
+    EditText etBill;
+    EditText etNumParty;
+    TextView tvTotalTip;
+    TextView tvTipPerPerson;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // initialize variables
+        etBill = (EditText)findViewById(R.id.editTextBill);
+        etNumParty = (EditText)findViewById(R.id.editTextNumParty);
+        tvTotalTip = (TextView)findViewById(R.id.textViewTotalTip);
+        tvTipPerPerson = (TextView)findViewById(R.id.textViewTipPerPerson);
+    }
+
+    public void onClickCalcButton(View view) {
+        Double totalTip = 0.0;
+        Double totalTipPerPerson = 0.0;
+        Double bill = Double.parseDouble(etBill.getText().toString());
+        Integer numParty = Integer.parseInt(etNumParty.getText().toString());
+
+        totalTip = bill * 0.1;
+        tvTotalTip.setText("Total Tip: " + totalTip.toString());
+
+        totalTipPerPerson = totalTip / numParty;
+        tvTipPerPerson.setText("Total Tip Per Person: " + totalTipPerPerson.toString());
+    }
+}
